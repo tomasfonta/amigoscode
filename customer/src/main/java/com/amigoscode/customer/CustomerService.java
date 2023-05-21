951613f9efc7b587d5dd60b9a18778562b17faa2
Tomas Fontanarrosa <tomas.fontanarrosa@edreamsodigeo.com>
2023-05-22 01:32:03 +0200

    Open Feign

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -1,11 +1,15 @@
 package com.amigoscode.customer;
 
+import com.amigoscode.clients.fraud.FraudCheckResponse;
+import com.amigoscode.clients.fraud.FraudClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 
 @Service
-public record CustomerService(CustomerRepository customerRepository, RestTemplate restTemplate) {
+public record CustomerService(CustomerRepository customerRepository,
+                              RestTemplate restTemplate,
+                              FraudClient fraudClient) {
 
     public void registerCustomer(CustomerRegistrationRequest request) {
 
@@ -16,17 +20,17 @@ public record CustomerService(CustomerRepository customerRepository, RestTemplat
                 .build();
 
         customerRepository.saveAndFlush(customer);
+
         //todo check if email valid
         // todo check if email not taken
 
-        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject("http://FRAUD/api/v1/fraud-check/{customerId}",
-                FraudCheckResponse.class,
-                customer.getId());
+        FraudCheckResponse fraudCheckResponse = fraudClient.isFraudster(customer.getId());
 
         if (fraudCheckResponse.isFraudster()) {
             throw new IllegalStateException("Fraudster");
         }
 
+        //todo: send notification
     }
 
 }
